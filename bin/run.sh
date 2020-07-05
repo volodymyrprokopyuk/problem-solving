e9bb587263eb1a5bd6922fd8128532c00151ae23
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2020-07-05 10:00:30 +0200

    test: add convert-size tests

--- bin/run.sh
+++ bin/run.sh
@@ -2,4 +2,5 @@
 
 readonly ROOT_DIR=$(pwd)
 
-guile3.0 -L "${ROOT_DIR}" linear-algorithm.scm "${@}"
+# guile3.0 -L "${ROOT_DIR}" linear-algorithm.scm "${@}"
+guile3.0 -L "${ROOT_DIR}" linear-algorithm-test.scm "${@}"
