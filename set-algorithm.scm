dca9dca04d4570945e4397db221635809c343072
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2020-09-04 08:16:24 +0200

    feat: add set algebra primitives

--- set-algorithm.scm
+++ set-algorithm.scm
@@ -1,6 +1,6 @@
 (define-module
   (set-algorithm)
-  #:export (both at-least-one neither empty-set empty-set? pick adjoin exclude))
+  #:export (both at-least-one neither set-empty set-empty? pick adjoin exclude))
 
 (use-modules
  (ice-9 curried-definitions)
@@ -75,35 +75,35 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Data abstraction primitives
 
-(define (empty-set)
+(define (set-empty)
   "Returns the empty set"
   '())
 
-(define (empty-set? s)
+(define (set-empty? s)
   "Returns #t if the set s is empty"
-  (eq? s (empty-set)))
+  (eq? s (set-empty)))
 
 (define (pick s)
   "Returns randomly selected element from the set s"
-  (when (empty-set? s) (error "pick: empty set"))
+  (when (set-empty? s) (error "pick: empty set"))
   (car s))
 
 (define (adjoin e s)
   "Adjoins the element e to the set s if the element is not already in the set"
   (let adjoin* ([ss s])
     (cond
-      [(empty-set? ss) (cons e s)]
+      [(set-empty? ss) (cons e s)]
       [(equal? e (car ss)) s]
       [else (adjoin* (cdr ss))])))
 
-;; (pp (adjoin 'a (empty-set)))
-;; (pp (adjoin 'b (adjoin 'a (empty-set))))
+;; (pp (adjoin 'a (set-empty)))
+;; (pp (adjoin 'b (adjoin 'a (set-empty))))
 
 (define ((exclude e) s)
   "Removes the element e from the set s if the element is in the set"
-  (let exclude* ([ss s] [r (empty-set)])
+  (let exclude* ([ss s] [r (set-empty)])
     (cond
-      [(empty-set? ss) s]
+      [(set-empty? ss) s]
       [(equal? e (car ss)) (append r (cdr ss))]
       [else (exclude* (cdr ss) (cons (car ss) r))])))
 
@@ -113,3 +113,80 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Data abstraction algorithms
+
+(define (make-set . e)
+  "Creates a set of the elements es"
+  (let make* ([e e] [r (set-empty)])
+    (if [null? e] r (make* (cdr e) (adjoin (car e) r)))))
+
+;; (pp (make-set 'a 'b 'c))
+
+(define ((set-every p) s)
+  "Returns #t if every element in the set s satisfies the predicate p"
+  (let every* ([s s])
+    (if [set-empty? s] #t
+        (let ([e (pick s)])
+          (if [p e] (every* ((exclude e) s)) #f)))))
+
+(define (set-every2 p)
+  "Implements set-every in terms of set-any"
+  (negate (set-any (negate p))))
+
+(define (set-every3 p)
+  "Implements set-every in terms of set-none"
+  (set-none (negate p)))
+
+;; (pp ((set-every even?) (make-set 2 4 6 8)))
+;; (pp ((set-every2 even?) (make-set 2 4 6 8)))
+;; (pp ((set-every3 even?) (make-set 2 4 6 8)))
+;; (pp ((set-every even?) (make-set 2 4 6 8 9)))
+;; (pp ((set-every2 even?) (make-set 2 4 6 8 9)))
+;; (pp ((set-every3 even?) (make-set 2 4 6 8 9)))
+
+(define ((set-any p) s)
+  "Returns #t if at least one (any) element in the set s satisfies the predicate p"
+  (let any* ([s s])
+    (if [set-empty? s] #f
+        (let ([e (pick s)])
+          (if [p e] #t (any* ((exclude e) s)))))))
+
+(define (set-any2 p)
+  "Implements set-any in termos of set-every"
+  (negate (set-every (negate p))))
+
+(define (set-any3 p)
+  "Implements set-any in termos of set-none"
+  (negate (set-none p)))
+
+;; (pp ((set-any even?) (make-set 1 3 5 7)))
+;; (pp ((set-any2 even?) (make-set 1 3 5 7)))
+;; (pp ((set-any3 even?) (make-set 1 3 5 7)))
+;; (pp ((set-any even?) (make-set 1 3 5 7 8)))
+;; (pp ((set-any2 even?) (make-set 1 3 5 7 8)))
+;; (pp ((set-any3 even?) (make-set 1 3 5 7 8)))
+
+(define ((set-none p) s)
+  "Returns #t if none of elemnts in the set s satisfies the predicate p"
+  (let none* ([s s])
+    (if [set-empty? s] #t
+        (let ([e (pick s)])
+          (if [p e] #f (none* ((exclude e) s)))))))
+
+(define (set-none2 p)
+  "Implements set-none in terms of set-every"
+  (set-every (negate p)))
+
+(define (set-none3 p)
+  "Implements set-none in terms of set-any"
+  (negate (set-any p)))
+
+;; (pp ((set-none even?) (make-set 1 3 5 7)))
+;; (pp ((set-none2 even?) (make-set 1 3 5 7)))
+;; (pp ((set-none3 even?) (make-set 1 3 5 7)))
+;; (pp ((set-none even?) (make-set 1 3 5 7 8)))
+;; (pp ((set-none2 even?) (make-set 1 3 5 7 8)))
+;; (pp ((set-none3 even?) (make-set 1 3 5 7 8)))
+
+(define ((set-equal? x) y)
+  "Returns #t if the sets x and y are equal"
+  'TODO)
