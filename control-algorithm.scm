f782fb12c3d4ef3cf0033b002faf424259cd3023
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2020-10-10 22:56:50 +0200

    feat: add escape from infinite loop

--- control-algorithm.scm
+++ control-algorithm.scm
@@ -18,7 +18,7 @@
 
 (define-condition-type &specific-error &base-error specific-error?)
 
-(call/cc
+#;(call/cc
  (lambda (k)
    (with-exception-handler
      (lambda (c) (format #t "ERROR: ~s\n" c) (k c))
@@ -29,7 +29,7 @@
                                  (reason "Base error reason")))])
               (raise c)))))))
 
-(guard
+#;(guard
  (c
   [(generic-error? c) (format #t "GENERIC ERROR: ~s\n" c)]
   [(specific-error? c) (format #t "SPECIFIC ERROR: ~s\n" c)]
@@ -41,7 +41,7 @@
                           (reason "Base error reason")))])
    (raise c)))
 
-(let* ([&base-error2 (make-condition-type '&base-error2 &condition
+#;(let* ([&base-error2 (make-condition-type '&base-error2 &condition
                                           '(code message reason))]
        [c (make-condition &base-error2 'code 'E0001
                           'message "Base error"
@@ -50,3 +50,29 @@
    (cc
     [else (format #t "ERROR 2: ~s" cc)])
    (raise c)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Continuation examples
+
+;; Escape from infinite loop
+(define (forever t)
+  "Applies the thunk t forever"
+  (t) (forever t))
+
+(define (infinite-loop)
+  (let ([c 0])
+    (forever (lambda () (pp c) (set! c (1+ c))))))
+
+;; (infinite-loop)
+
+(define (escape-infinite-loop)
+  (call/cc
+   (lambda (k)
+     (let ([c 0])
+       (forever
+        (lambda ()
+          (cond
+            [(> c 10) (k 'done)]
+            [else (pp c) (set! c (1+ c))])))))))
+
+;; (pp (escape-infinite-loop))
