8f065a37f6fd368bacd8d355a6ce5a5c0c2fbd39
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2020-12-22 13:22:35 +0100

    feat: add fork and block-fork strategies

--- gauche/tic-tac-toe.scm
+++ gauche/tic-tac-toe.scm
@@ -7,7 +7,11 @@
 (use gauche.parameter)
 (use gauche.parseopt)
 
+;; Random
+
 (random-source-randomize! default-random-source)
+
+;; Constant
 
 (define-constant name "Tic-tac-toe")
 (define-constant description "Noughts and corsses game. To win put three marks in a row \
@@ -16,45 +20,91 @@
 (define-constant author "Volodymyr Prokopyuk")
 (define-constant year "2020")
 
+;; Error
+
 (define-condition-type <app-error> <error> app-error? (message message))
 (define-condition-type <input-error> <app-error> input-error?)
 
 (define (report-error e)
-  "Display the error e message on the stderr"
+  "Shows the error e message on the stderr"
   (display #"Error: ~(message e)\n" (current-error-port)))
 
-(define (random-strategy b)
-  "Realizes random program strategy"
-  (let* ([a (vector-ec (:vector c (index i) b) (if [#[_] c]) i)]
-         [i (random-integer (vector-length a))])
-    (vector-ref a i)))
-
-(define (scan-board b f)
-  "Scans the board b rows with the function f and returns the first non-#f result"
-  (any f '((0 1 2) (3 4 5) (6 7 8) (0 3 6) (1 4 7) (2 5 8) (0 4 8) (2 4 6))))
-
-(define (about-to-win b r m)
-  "Returns the missing index in the row r for the mark m to win on the board b, \
-   otherwise #f"
-  (and (= (fold-ec 0 (:vector c (index i) b) (and (memv i r) (char=? c m)) 1 +) 2)
-       (first-ec #f (:vector c (index i) b) (and [memv i r] [#[_] c]) i)))
-
-(define (win-strategy b)
-  "Relizes win program strategy if possible, otherwise returns #f"
-  (scan-board b (cut about-to-win b <> (program-mark))))
-
-(define (configure-strategy s)
-  "Configures program strategy based on the strategy s command line option"
-  (case s
-    [(easy) random-strategy]
-    ((hard) (any-pred win-strategy random-strategy))
-    [else random-strategy]))
+;; Mark
 
 (define (string->mark s)
-  "Converts the string s to player mark character either #\X or #\O"
+  "Converts the string s to a mark char-set either #[X] or #[O]"
   (when [zero? (string-length s)] (error <input-error> :message "empty mark"))
   (let ([c ($ char-upcase $ string-ref s 0)])
-    (if [#[XO] c] c (error <input-error> :message #"invalid mark ~c"))))
+    (if [#[XO] c] (char-set c) (error <input-error> :message #"invalid mark ~c"))))
+
+(define (mark->char m)
+  "Converts the mark m either #[X] or #[O] into a corresponding character"
+  (if [m #\X] #\X #\O))
+
+(define (opposite-mark m)
+  "Returns #[X] when the mark m is #[O] and viceversa"
+  (if [m #\X] (char-set #\O) (char-set #\X)))
+
+(define (program-mark)
+  "Returns the opposite to the player mark parameter"
+  (opposite-mark (player-mark)))
+
+(define (empty-mark)
+  "Returns the empty mark #[_]"
+  #[_])
+
+;; Board
+
+(define (make-board)
+  "Creates an empty board with 9 empty cells"
+  (make-vector 9 #\_))
+
+(define (mark! b i m)
+  "Sets the mark m #\X or #\O on the board b at the index i"
+  (set! (~ b i) m))
+
+(define (board->string b)
+  "Converts the board b into the string representation with 3-cells rows"
+  (string-append-ec
+   (:vector c (index i) b)
+   (string c (if [zero? (remainder (+ i 1) 3)] #\newline #\space))))
+
+(define (setup-board)
+  "Makes a board and makes the first mark if the program goes first"
+  (let ([b (make-board)])
+    (cond
+      [(player-first) b]
+      [else (mark! b ((program-strategy) b) ($ mark->char $ program-mark)) b])))
+
+;; Check board
+
+(define (cell-marked? b i)
+  "Returns #t if the cell at the index i is already marked on the board b"
+  (any?-ec (:vector c (index j) b) (and [= i j] (not [(empty-mark) c]))))
+
+(define (row-marked? b r m)
+  "Returns #t if the row r is marked with the mark m on the board b"
+    (every?-ec (:vector c (index i) b) (if [memv i r]) [m c]))
+
+(define (win? b m)
+  "Returns #t if the there is a row on the board b marked with the mark m"
+  (scan-rows b (cut row-marked? b <> m)))
+
+(define (complete? b)
+  "Returns #t if all the cells the board b are marked"
+  (every?-ec (:vector c b) (not [(empty-mark) c])))
+
+(define (check-board b m play*)
+  "Checks the board b for the win of the mark m or draw, otherway continues to play*"
+  (let* ([player? (equal? m (player-mark))]
+         [s (if player? "player" "program")])
+    (cond
+      [(win? b m) (display (board->string b)) (display #"~s won\n")]
+      [(complete? b) (display (board->string b)) (display "draw\n")]
+      [player? (program-turn b play*)]
+      [else (play* b)])))
+
+;; Strategy
 
 (define (validate-strategy s)
   "Validates supported strategies"
@@ -62,15 +112,104 @@
     (error <input-error> :message #"invalid strategy ~s"))
   s)
 
+;; Random strategy
+
+(define (random-strategy b)
+  "Returns the index of a randomly selected cell from the available empty cells"
+  (let* ([e (vector-ec (:vector c (index i) b) (if [(empty-mark) c]) i)]
+         [i (random-integer (vector-length e))])
+    (vector-ref e i)))
+
+;; Row-based strategy
+
+(define (scan-rows b f)
+  "Scans the board b rows (horizontal, vectical, diagonal) with the function f \
+   and returns the first non-#f result"
+  (any f '((0 1 2) (3 4 5) (6 7 8) (0 3 6) (1 4 7) (2 5 8) (0 4 8) (2 4 6))))
+
+(define (about-to-win b r m)
+  "Returns the missing index in the row r for the mark m to win on the board b, \
+   otherwise #f"
+  (and (= (fold-ec 0 (:vector c (index i) b) (and [memv i r] [m c]) 1 +) 2)
+       (first-ec #f (:vector c (index i) b) (and [memv i r] [(empty-mark) c]) i)))
+
+(define (win-strategy b)
+  "Returns the index of the program one-cell-missing to win row, otherwise #f"
+  (scan-rows b (cut about-to-win b <> (program-mark))))
+
+(define (block-win-strategy b)
+  "Returns the index of the player one-cell-missing to win row, otherwise #f"
+  (scan-rows b (cut about-to-win b <> (player-mark))))
+
+;; Corner-based strategy
+
+(define (scan-corners b f)
+  "Scans the board b corners (cental cross, square corners) with the \
+   function f and returns the first non-#f result"
+  (any f '((1 7 4 3 5) (0 1 2 5 8) (2 5 8 7 6) (8 7 6 3 0) (6 3 0 1 2))))
+
+(define (two-non-blocked b r m)
+  "Returns the index of the corner r with two non-blocked rows for the mark m \
+   on the board b, otherwise #f"
+  (and (every?-ec (:vector c (index i) b) (if [memv i r]) (not [(opposite-mark m) c]))
+       (every?-ec (:vector c (index i) b) (if [= i (caddr r)]) [(empty-mark) c])
+       (caddr r)))
+
+(define (fork-strategy b)
+  "Returns the index of the corner with two program non-blocked rows"
+  (scan-corners b (cut two-non-blocked b <> (program-mark))))
+
+(define (block-fork-strategy b)
+  "Returns the index of the corner with two player non-blocked rows"
+  (scan-corners b (cut two-non-blocked b <> (player-mark))))
+
+(define (configure-strategy s)
+  "Configures the program strategy based on the strategy s command line option"
+  (case s
+    [(easy) (any-pred win-strategy random-strategy)]
+    ((hard) (any-pred win-strategy block-win-strategy random-strategy))
+    ((impossible)
+     (any-pred win-strategy block-win-strategy fork-strategy block-fork-strategy
+               random-strategy))
+    [else random-strategy]))
+
+(define (program-strategy)
+  "Returns configured program strategy"
+  (configure-strategy (strategy)))
+
+;; Help
+
+(define (program-help)
+  "Returns the program name, description, author, year and command line option"
+  #"~name version ~version, ~author, ~year
+~description
+
+Usage: tic-tac-toe [--help | --version ] [--first --mark <m> --strategy <s>]
+
+Options:
+  -h, --help          Show help
+  -v, --version       Show version
+  -f, --first         Program first (player first default)
+  -m, --mark <m>      Set player mark: X (default), O
+  -s, --startegy <s>  Select strategy: easy (default), hard, impossible
+")
+
+(define (game-help)
+  "Returns the tic-tac-toe commands descirption"
+  #"(q)uit, (h)elp, (v)ersion, (r)eset, (1-9) marks\n")
+
+;; Parameter
+
 (define help? (make-parameter #f))
 (define version? (make-parameter #f))
 (define player-first (make-parameter #t))
 (define player-mark (make-parameter "X" string->mark))
 (define strategy (make-parameter 'easy validate-strategy))
-(define program-strategy (make-parameter (configure-strategy (strategy))))
+
+;; Configuration
 
 (define (configure args)
-  "Configures tic-tac-toe by reading command line options"
+  "Configures the tic-tac-toe program from the command line options"
   (let-args args
     ([h "h|help"]
      [v "v|version"]
@@ -82,92 +221,26 @@
     (version? v)
     (player-first (not f))
     (and m (player-mark m))
-    (display #"player mark ~(player-mark)\n")
+    (display #"player mark ~($ mark->char $ player-mark)\n")
     (and s (strategy s))
-    (display #"program strategy ~(strategy)\n")
-    (program-strategy (configure-strategy (strategy)))))
+    (display #"program strategy ~(strategy)\n")))
 
-(define (program-help)
-  "Returns program name, description, author, year and command line options"
-  #"~name version ~version, ~author, ~year
-~description
-
-Usage: tic-tac-toe [--help | --version ] [--mark <m> --strategy <s>]
-
-Options:
-  -h, --help          Show help
-  -v, --version       Show version
-  -f, --first         Program first
-  -m, --mark <m>      Set player mark: X (default), O
-  -s, --startegy <s>  Select strategy: easy (default), hard, impossible
-")
-
-(define (game-help)
-  "Returns game commands descirption"
-  #"(q)uit, (h)elp, (v)ersion, (r)eset, (1-9) marks\n")
-
-(define (make-board)
-  "Creates an empty 9-cells board"
-  (make-vector 9 #\_))
-
-(define (mark! b i m)
-  "Sets the mark m (X or O) on the board b at the index i"
-  (set! (~ b i) m))
-
-(define (board->string b)
-  "Converts the board b into a string representation with 3-cells rows"
-  (string-append-ec
-   (:vector c (index i) b)
-   (string c (if [zero? (remainder (+ i 1) 3)] #\newline #\space))))
-
-(define (program-mark)
-  "Returns program mark opposite to player mark"
-  (if [#[X] (player-mark)] #\O #\X))
-
-(define (setup-board)
-  "Makes board and makes the first mark if the program hoes first"
-  (let ([b (make-board)])
-    (cond
-      [(player-first) b]
-      [else (mark! b ((program-strategy) b) (program-mark)) b])))
-
-(define (cell-marked? b i)
-  "Returns #t if the cell at the index i is already marked on the board b"
-  (any?-ec (:vector c (index j) b) (and [#[XO] c] [= i j])))
-
-(define (row-marked? b r m)
-  "Returns #t if the row r is marked with the mark m on the board b"
-    (every?-ec (:vector c (index i) b) (if (memv i r)) [char=? c m]))
-
-(define (win? b m)
-  "Returns #t if the there is a row on the board b marked with the mark m"
-  (scan-board b (cut row-marked? b <> m)))
-
-(define (complete? b)
-  "Returns #t if the board b is complete"
-  (every?-ec (:vector c b) [#[^_] c]))
-
-(define (check-board b m play*)
-  "Checks the board b for winner or draw the the mark m, otherway continues to play*"
-  (let* ([player? (char=? m (player-mark))]
-         [s (if player? "player" "program")])
-    (cond
-      [(win? b m) (display (board->string b)) (display #"~s won\n")]
-      [(complete? b) (display (board->string b)) (display "draw\n")]
-      [player? (program-turn b play*)]
-      [else (play* b)])))
+;; Play
 
 (define (player-turn b c play*)
   "Realizes player's turn"
   (let ([i (- (digit->integer c) 1)])
     (cond
       [(cell-marked? b i) (display #"~(+ i 1) already marked\n") (play* b)]
-      [else (mark! b i (player-mark)) (check-board b (player-mark) play*)])))
+      [else
+       (mark! b i ($ mark->char $ player-mark))
+       (check-board b (player-mark) play*)])))
 
 (define (program-turn b play*)
   "Realizes program's turn"
   (let ([i ((program-strategy) b)])
-    (mark! b i (program-mark)) (check-board b (program-mark) play*)))
+    (mark! b i ($ mark->char $ program-mark))
+    (check-board b (program-mark) play*)))
 
 (define (play)
   (define (prompt) (display "> ") (flush))
@@ -176,7 +249,7 @@ Options:
       (cond
         [(or (eof-object? c) (#[^ \n] c)) c]
         [else (read-command)])))
-  "Plays tic-tac-toe by reading commands entered by the player"
+  "Plays the tic-tac-toe by reading commands entered by the player"
   (let play* ([b (setup-board)])
     (display (board->string b))
     (prompt)
@@ -189,12 +262,10 @@ Options:
         [(#[1-9] c) (player-turn b c play*)]
         [else (display #"error ~c\n") (play* b)]))))
 
+;; Main
+
 (define (main args)
-  #;(let ([b (make-board)])
-    (mark! b 2 #\X)
-    (mark! b 8 #\X)
-    (display (board->string b))
-    #?=(win-strategy b))
+  "Starts program execution"
   (guard
    (e
     [(<input-error> e) (report-error e) 1])
