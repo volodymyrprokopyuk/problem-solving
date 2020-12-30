f60c2ce0c85def1bf392b99d0552dec39dfa0e19
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2020-12-30 12:18:50 +0100

    feat: add overlap and doubles algorithms

--- gauche/recursive-algorithm.scm
+++ gauche/recursive-algorithm.scm
@@ -37,8 +37,8 @@
                 [else (downup* (cdr l) (cons (reverse l) r))]))])
        (append d ($ cdr $ reverse d)))]))
 
-;; #?=(downup '(a b c d))
 ;; #?=(downup '())
+;; #?=(downup '(a b c d))
 
 (define (downup2 l)
   "Returns a list of sublists of the list l by dopping down and then by adding up"
@@ -47,8 +47,8 @@
     [(= (length l) 1) (list l)]
     [else (append (list l) (downup2 (drop-right l 1)) (list l))]))
 
-;; #?=(downup2 '(a b c d))
 ;; #?=(downup2 '())
+;; #?=(downup2 '(a b c d))
 
 (define (pig s)
   "Translates a word into the pig language"
@@ -81,13 +81,71 @@
 
 (define (overlapped-pairs l)
   "Returns a list of overlapped pairs from the list l"
-  (cond
-    [(< (length l) 3) (list l)]
-    [else
-     (let overlap* ([l (cdr l)] [a (car l)] [r '()])
-       (cond
-         [(null? l) (reverse r)]
-         [else (overlap* (cdr l) (car l) (cons (list a (car l)) r))]))]))
+  (let overlap* ([l l] [r '()])
+    (cond
+      [(< (length l) 2) (reverse r)]
+      [else (overlap* (cdr l) (cons (take l 2) r))])))
 
 ;; #?=(overlapped-pairs '(a b))
 ;; #?=(overlapped-pairs '(a b c d))
+
+(define (non-overlapped-pairs l)
+  "Returns a list of non-overlapped pairs of the list l"
+  (let non-overlap* ([l l] [r '()])
+    (cond
+      [(< (length l) 3) ($ reverse $ cons l r)]
+      [else (non-overlap* (cddr l) (cons (take l 2) r))])))
+
+;; #?=(non-overlapped-pairs '(a b))
+;; #?=(non-overlapped-pairs '(a b c d e))
+
+(define (=reverse l)
+  "Returns a reversed list of the list l"
+  (cond
+    [(null? l) l]
+    #;[else (append (=reverse (cdr l)) (list (car l)))]
+    [else (cons (last l) (=reverse (drop-right l 1)))]))
+
+;; #?=(=reverse '(a b c d))
+
+(define (evens l)
+  "Returns the elements at even positions from the list l"
+  (let evens* ([l l] [i 1] [r '()])
+    (cond
+      [(null? l) (reverse r)]
+      [(even? i) (evens* (cdr l) (+ i 1) (cons (car l) r))]
+      [else (evens* (cdr l) (+ i 1) r)])))
+
+;; #?=(evens '())
+;; #?=(evens '(1 2 3 4 5 6 7))
+
+(define (evens2 l)
+  "Returns the elements at even positions from the list l"
+  (let evens* ([l l] [r '()])
+    (cond
+      [(< (length l) 2) (reverse r)]
+      [else (evens* (cddr l) (cons (cadr l) r))])))
+
+;; #?=(evens2 '())
+;; #?=(evens2 '(1 2 3 4 5 6 7))
+
+(define (down l)
+  "Returns the list of sublists of the list l"
+  (cond
+    [(null? l) '()]
+    [else (cons l (down (drop-right l 1)))]))
+
+;; #?=(down '())
+;; #?=(down '(a b c d))
+
+(define (filter-doubles s)
+  "Returns a list of double letter strings from the string s"
+  (let filter* ([l (string->list s)] [r '()])
+    (cond
+      [(< (length l) 2) (reverse r)]
+      [(every (cut char=? <> (car l)) (take l 2))
+       (filter* (cdr l) (cons ($ list->string $ take l 2) r))]
+      [else (filter* (cdr l) r)])))
+
+;; #?=(filter-doubles "bookkeeper")
+;; #?=(filter-doubles "mississippi")
