41ede2c2ebb50ff9977f4e849522d77af2eabe36
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2021-01-01 11:41:46 +0100

    feat: add spell-number algorithm

--- gauche/bin/run.sh
+++ gauche/bin/run.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 readonly ROOT_DIR=$(pwd)
-readonly SRFI_DIR=$HOME/Projects/scheme-upgrade
 
 # readonly SOURCE=playground-algorithm
 # readonly SOURCE=shell-algorithm
@@ -9,12 +8,14 @@ readonly SRFI_DIR=$HOME/Projects/scheme-upgrade
 # readonly SOURCE=continuation-algorithm
 # readonly SOURCE=iterative-algorithm
 # readonly SOURCE=conditional-algorithm
-readonly SOURCE=recursive-algorithm
+# readonly SOURCE=recursive-algorithm
+
+# gosh "${SOURCE}.scm" "${@}"
+
 # readonly SOURCE=tic-tac-toe
 # readonly SOURCE=recambios
+readonly SOURCE=spell-number
 
-export GAUCHE_LOAD_PATH="${ROOT_DIR}:${SRFI_DIR}"
-
-gosh "${SOURCE}.scm" "${@}"
 # gosh -m tic-tac-toe "${SOURCE}.scm" "${@}"
 # gosh -m recambios "${SOURCE}.scm" "${@}"
+gosh -m spell-number "${SOURCE}.scm" "${@}"
