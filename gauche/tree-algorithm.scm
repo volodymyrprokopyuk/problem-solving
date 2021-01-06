0d530e4958f9e488d0e71fa2238ec10e4946a19f
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2021-01-06 12:18:37 +0100

    feat: add tree recursion and mutual recursion algorithms

--- gauche/tree-algorithm.scm
+++ gauche/tree-algorithm.scm
@@ -1,10 +1,10 @@
 (define (make-node d c)
   "Returns a node (tree) with the datum d and children c"
-  (list d c))
+  (cons d c))
 
 (define (make-leaf d)
   "Returns a node with the datum d and no children"
-  (list d '()))
+  (list d))
 
 ;; #?=(make-node 'a (map make-leaf '(b c)))
 
@@ -16,7 +16,7 @@
 
 (define (children n)
   "Returns children of the node n"
-  (cadr n))
+  (cdr n))
 
 ;; #?=(children (make-node 'a (map make-leaf '(b c))))
 
@@ -25,12 +25,46 @@
   (null? (children n)))
 
 (define (count-leaves n)
-  "Returns the numbe of leaves in the node n"
+  "Returns the number of leaves in the node n (higher-order function map)"
   (cond
     [(leaf? n) 1]
     [else (apply + (map count-leaves (children n)))]))
 
-(let ([n (make-node 'a
+(define (count-leaves2 n)
+  "Returns the number of leaves in the node n (mutual recursion)"
+  (cond
+    [(leaf? n) 1]
+    [else
+     (let count* ([c (children n)])
+       (cond
+         [(null? c) 0]
+         [else (+ (count-leaves2 (car c)) (count* (cdr c)))]))]))
+
+(define (in-tree? d n :optional (e eq?))
+  "Returns the first node of the tree n whose datum is equal to the datum d"
+  (cond
+    [(e (datum n) d) n]
+    [else (any (cut in-tree? d <> e) (children n))]))
+
+(define (in-tree2? d n :optional (e eq?))
+  "Returns the first node of the tree n whose datum is equal to the datum d"
+  (cond
+    [(e (datum n) d) n]
+    [else
+     (let in-tree* ([c (children n)])
+       (cond
+         [(null? c) #f]
+         [else (or (in-tree2? d (car c) e) (in-tree* (cdr c)))]))]))
+
+(define (locate d n :optional (e eq?))
+  "Returns a path to the node whose datum is equal to the datum d, otherwise '()"
+  (reverse
+   (let locate* ([n n] [r '()])
+     (cond
+       [(e (datum n) d) (cons (datum n) r)]
+       [else (any (cut locate* <> (cons (datum n) r)) (children n))]))))
+
+#;(let ([n (make-node 'a
                     (list
                      (make-node 'b
                                 (list
@@ -39,4 +73,8 @@
                      (make-node 'c
                                 (list
                                  (make-node 'f (map make-leaf '(k l m)))))))])
-  #?=(count-leaves n))
+  #?=(count-leaves n)
+  #?=(count-leaves2 n)
+  #?=(in-tree? 'e n)
+  #?=(in-tree2? 'e n)
+  #?=(locate 'l n))
