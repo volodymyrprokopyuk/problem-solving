5e7a0d18557faaa793a316454d2069c1038e690e
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2021-01-08 22:03:50 +0100

    refactor: refactor playground database scripts

--- postgresql/bin/run.sh
+++ postgresql/bin/run.sh
@@ -3,34 +3,35 @@
 set -eu
 
 readonly ROOT_DIR=$(pwd)
-export PGDATABASE=playground
+# export PGDATABASE=playground
+export PGDATABASE=booking
 export PGUSER=vlad
 
-function playground_schema {
+function create_schema {
     psql -c "DROP DATABASE IF EXISTS $PGDATABASE WITH (FORCE);" \
          -c "CREATE DATABASE $PGDATABASE WITH OWNER $PGUSER;" postgres
-    psql -f $ROOT_DIR/playground-schema.sql
+    psql -f $ROOT_DIR/$PGDATABASE/schema.sql
 }
 
-function playground_data {
-    psql -f $ROOT_DIR/playground-data.sql
+function load_data {
+    psql -f $ROOT_DIR/$PGDATABASE/data.sql
 }
 
-function playground_query {
-    psql -f $ROOT_DIR/playground-query.sql
+function execute_query {
+    psql -f $ROOT_DIR/$PGDATABASE/query.sql
 }
 
 case $1 in
     -s|--scheme)
-        playground_schema
+        create_schema
         exit
         ;;
     -d|--data)
-        playground_data
+        load_data
         exit
         ;;
     -q|--query)
-        playground_query
+        execute_query
         exit
         ;;
     *)
