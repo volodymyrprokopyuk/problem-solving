5c926752edd4809a3a2d29b9f63b3645828e94ff
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2021-05-23 09:43:10 +0200

    feat: add recursive algorithms

--- gauche/concrete-abstractions.scm
+++ gauche/concrete-abstractions.scm
@@ -38,3 +38,59 @@
 
 ;; #?=(power 2 0)
 ;; #?=(power 2 3)
+
+;; Mathematical induction
+;; 1. Base case is correct by definition x = 0
+;; 2. Induction hypothesis assumes correctness of x - 1
+;; 3. Inductive step proves correctness of x
+(define (square x)
+  (cond [(zero? x) 0] [else (+ (square (- x 1)) (* 2 x) -1)]))
+
+;; #?=(square 2)
+;; #?=(square 3)
+
+(define (=quotient x y)
+  (let quotient* ([x x] [r 0])
+    (cond
+      [(< x 0) (- (=quotient (- x) y))]
+      [(< y 0) (- (=quotient x (- y)))]
+      [(< x y) r]
+      [else (quotient* (- x y) (+ r 1))])))
+
+;; #?=(=quotient 3 4)
+;; #?=(=quotient -4 4)
+;; #?=(=quotient 5 -4)
+;; #?=(=quotient -9 -4)
+
+(define (mul x y)
+  (let mul* ([y y] [r 0])
+    (cond
+      [(< x 0) (- (mul (- x) y))]
+      [(< y 0) (- (mul x (- y)))]
+      [(zero? y) r]
+      [else (mul* (- y 1) (+ r x))])))
+
+;; #?=(mul 2 0)
+;; #?=(mul -2 1)
+;; #?=(mul 2 -2)
+;; #?=(mul -2 -3)
+
+(define (number-of-digits n :optional (b 10))
+  (let nod* ([n n] [r 1])
+    (cond
+      [(negative? n) (number-of-digits (- n))]
+      [(< n b) r]
+      [else (nod* (quotient n b) (+ r 1))])))
+
+;; #?=(number-of-digits 0)
+;; #?=(number-of-digits 12)
+;; #?=(number-of-digits -345)
+
+(define (integer-factorization x)
+  (let fact* ([x x] [n 0])
+    (cond [(odd? x) (values n x)] [else (fact* (quotient x 2) (+ n 1))])))
+
+(for-each
+ (lambda (x)
+   (receive (n k) (integer-factorization x) (format #t "~a = 2^~a ~a\n" x n k)))
+ '(1 2 3 4 5 6 7 8 9 10))
