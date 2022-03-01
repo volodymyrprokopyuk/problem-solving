6a515aaedfa9591834f3361f5ed6145c355748ed
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2022-03-01 11:53:31 +0100

    feat(nim): add threads examples

--- nim/bin/run.sh
+++ nim/bin/run.sh
@@ -3,10 +3,10 @@
 # SRC=regex.nim
 # SRC=json.nim
 # SRC=options.nim
+SRC=threads.nim
 #
 # SRC=tutorial.nim
 # SRC=salewski.nim
-SRC=nim_in_action.nim
+# SRC=nim_in_action.nim
 
-# exec nim --verbosity:0 --threads:on --gc:orc r $SRC $@
 exec nim --verbosity:0 --threads:on r $SRC $@
---
+++ nim/threads.nim
@@ -0,0 +1,52 @@
+import std/[os, threadpool]
+
+# createThread, joinThreads = strictly parallel (uses multicore CPU)
+# spawn, ^ = strictly parallel (uses multicore CPU)
+# {.async.}, await = strictly concurrent (uses single-threaded event loop)
+
+# threads = long-running tasks that communicate with other threads periodically
+# Each thread has its own GC and heap and sharing of memory is restricted
+# To use the most of CPU power create one thread per CPU core
+# Unhandlad exception in any thread terminates the whole process
+
+var th1, th2: Thread[Slice[int]] # Slice param for the thread procedure
+
+proc count(slice: Slice[int]) {.thread.} = # thread procedure, no return value
+  # thread-local finish handler
+  onThreadDestruction(proc() = echo "Finished ", slice)
+  for i in slice:
+    echo i
+    sleep(500)
+
+th1.createThread(count, 0..4) # deterministic thread execution, full control
+th2.createThread(count, 5..9) # starts thread execution with procedure + param
+echo th1.running, " ", th2.running # checks thread running status
+joinThreads(th1, th2) # waits for all threads to finish
+
+
+# threadpool = short-living tasks that produce a result (no communication with
+# other threads)
+
+proc countAndResult(slice: Slice[int]): int = # thread procedure + return value
+  for i in slice:
+    echo i
+    sleep(500)
+  slice.b
+
+let
+  # Enqueues a task + param to the thread pool (non-deterministic execution)
+  # Always spawns a new task. The task is never execute in the calling thread
+  result1: FlowVar[int] = spawn countAndResult(0..4) # data flow variable
+  result2: FlowVar[int] = spawn countAndResult(5..9)
+
+# Alternative 1
+sync() # waits for all spawned tasks (optional)
+echo ^result1, " ", ^result2 # blocks and retrievs FlowVar result
+
+# Alternative 2
+while not result1.isReady: # checks task result readiness
+  echo "Waiting"
+  sleep(1000)
+echo "Result ", ^result1
+# blocks and passes the result
+result2.awaitAndThen(proc(b: int) = echo "Result ", b)
