cba31226f90214ca86f0248e924b01951ec72c5c
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2023-04-07 17:14:37 +0200

    feat(js): add BST

--- js/playground/irp.js
+++ js/playground/irp.js
@@ -112,3 +112,113 @@ function hornerPolynomial(arr, x) {
 // [[1], [1, 2], [1, 1, 2, 3]].forEach(arr =>
 //   console.log(hornerPolynomial(arr, 2))
 // )
+
+// O(n)
+function containsDigit(n, d) {
+  if (n < 10) { return n === d }
+  else { return n % 10 === d || containsDigit(Math.floor(n / 10), d) }
+}
+
+// O(n) tail-recursive
+function containsDigit2(n, d) {
+  if (n < 10) { return n === d }
+  else if (n % 10 === d) { return true }
+  else { return containsDigit2(Math.floor(n / 10), d) }
+}
+
+// [1, 12, 123].forEach(n => console.log(containsDigit2(n, 2)))
+
+// O(n)
+function equalString(a, b) {
+  if (a.length !== b.length) { return false }
+  else if (a === "") { return true }
+  else { return a[0] === b[0] && equalString(a.slice(1), b.slice(1)) }
+}
+
+// O(n) tail-recursive
+function equalString2(a, b) {
+  if (a.length !== b.length) { return false }
+  else if (a === "") { return true }
+  else if (a[0] !== b[0]) { return false }
+  else { return equalString2(a.slice(1), b.slice(1)) }
+}
+
+// [["", ""], ["", "a"], ["a", "b"], ["abc", "abc"]].forEach(ss =>
+//   console.log(equalString2(...ss))
+// )
+
+// O(n) tail-recursive
+function linearSearch(arr, x, i = 0) {
+  if (i === arr.length) { return -1 }
+  else if (arr[i] === x) { return i }
+  else { return linearSearch(arr, x, ++i) }
+}
+
+// [[[], 1], [[1], 1], [[1, 2, 3], 2], [[1, 2], 3]].forEach(([arr, x]) =>
+//   console.log(linearSearch(arr, x))
+// )
+
+// O(log(n)) tail-recursive
+function binarySearch(arr, x, a = 0, b = arr.length - 1) {
+  if (a > b) { return -1 }
+  const m = a + Math.floor((b - a) / 2)
+  if (arr[m] === x) { return m }
+  else if (x < arr[m]) { return binarySearch(arr, x, a, m - 1) }
+  else { return binarySearch(arr, x, m + 1, b) }
+}
+
+// [[[], 1], [[1, 2], 3], [[1, 2, 3, 4], 2]].forEach(([arr, x]) =>
+//   console.log(binarySearch(arr, x))
+// )
+
+class TNode {
+  key; data
+  left = null; right = null
+
+  constructor(key, data) {
+    this.key = key; this.data = data
+  }
+}
+
+class BSTree {
+  #root; length = 0
+
+  // O(log(n)) tail-recursive
+  set(key, data, nd = this.#root) {
+    if (!this.length) {
+      this.#root = new TNode(key, data); ++this.length
+    } else if (key < nd.key) {
+      if (!nd.left) {
+        nd.left = new TNode(key, data); ++this.length
+      } else { this.set(key, data, nd.left) }
+    } else {
+      if (!nd.right) {
+        nd.right = new TNode(key, data); ++this.length
+      } else { this.set(key, data, nd.right) }
+    }
+  }
+
+  // O(log(n)) tail-recursive
+  get(key, nd = this.#root) {
+    if (!nd) { return }
+    else if (nd.key === key) { return nd }
+    else if (key < nd.key) { return this.get(key, nd.left) }
+    else { return this.get(key, nd.right) }
+  }
+
+  get inOrder() {
+    function* inOrderGen(nd) {
+      if (nd) {
+        yield* inOrderGen(nd.left)
+        yield nd
+        yield* inOrderGen(nd.right)
+      }
+    }
+    return { [Symbol.iterator]: () => inOrderGen(this.#root) }
+  }
+}
+
+// const tr = new BSTree();
+// [5, 2, 3, 6, 1, 4].forEach(key => tr.set(key, key.toString()))
+// for (const nd of tr.inOrder) { console.log(nd.key, nd.data) }
+// [1, 4, 6, 2, 5, 0].forEach(key => console.log(tr.get(key)))
