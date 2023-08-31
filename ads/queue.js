7e0cbd1a34950dbdacfebc671d537544648524a9
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2023-08-31 15:58:19 +0200

    feat(ads): add Deque

--- ads/queue.js
+++ ads/queue.js
@@ -43,9 +43,9 @@ export class Queue {
   deq() {
     if (this.#length === 0) { error("dequeue from empty queue") }
     const nd = this.front
-    this.front = this.front.next
     --this.#length
     if (this.#length === 0) { this.front = this.rear = null }
+    else { this.front = this.front.next }
     return nd.data
   }
 
@@ -56,12 +56,81 @@ export class Queue {
   }
 }
 
-// const que = new Queue()
-// que.enq(1).enq(2).enq(3).enq(4)
-const que = Queue.from([5, 6, 7, 8])
-console.log(que, que.length)
-while(que.length > 0) { console.log(que.deq()) }
-que.enq(9).enq(10)
-console.log(que.deq())
-console.log(que.peek())
-console.log(que, que.length)
+export class Deque {
+  front = null
+  rear = null
+  #length = 0
+
+  get length() { return this.#length }
+
+  static from(it) {
+    const deq = new Deque()
+    for (const el of it) { deq.enq(el) }
+    return deq
+  }
+
+  [Symbol.iterator]() {
+    let nd = this.front
+    const next = () => {
+      if (nd) {
+        const value = nd.data
+        nd = nd.next
+        return { value, done: false }
+      }
+      return { done: true }
+    }
+    return { next }
+  }
+
+  [inspect.custom]() { return `Deque(${Array.from(this)})` }
+
+  // O(1)
+  enq(data) {
+    const nd = new LNode(data)
+    if (this.#length === 0) { this.front = this.rear = nd }
+    else { nd.prev = this.rear; this.rear = this.rear.next = nd }
+    ++this.#length
+    return this
+  }
+
+  // O(1)
+  enqFront(data) {
+    const nd = new LNode(data)
+    if (this.#length === 0) { this.front = this.rear = nd }
+    else { nd.next = this.front; this.front = this.front.prev = nd }
+    ++this.#length
+    return this
+  }
+
+  // O(1)
+  deq() {
+    if (this.#length === 0) { error("deq from empty deque") }
+    const nd = this.front
+    --this.#length
+    if (this.#length === 0) { this.front = this.rear = null }
+    else { this.front = this.front.next; this.front.prev = null }
+    return nd.data
+  }
+
+  // O(1)
+  deqRear() {
+    if (this.#length === 0) { error("deqRear from empty deque") }
+    const nd = this.rear
+    --this.#length
+    if (this.#length === 0) { this.front = this.rear = null }
+    else { this.rear = this.rear.prev; this.rear.next = null }
+    return nd.data
+  }
+
+  // O(1)
+  peek() {
+    if (this.#length === 0) { error("peek from empty deque") }
+    return this.front.data
+  }
+
+  // O(1)
+  peekRear() {
+    if (this.#length === 0) { error("peekRear from empty deque") }
+    return this.rear.data
+  }
+}
