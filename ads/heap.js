5420f72c3318d740535d5dbd70a29176afc40ab4
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2023-09-14 12:52:43 +0200

    rfct(ads): refactor Heap

--- ads/heap.js
+++ ads/heap.js
@@ -10,30 +10,31 @@ export class Heap {
   get length() { return this.#arr.length }
 
   static from(it, cmp) {
-    const heap = cmp ? new Heap(cmp) : new Heap()
-    for (const el of it) { heap.push(el) }
+    const heap = new Heap(cmp)
+    for (const el of it) {
+      Array.isArray(el) ? heap.push(el[0], el[1]) : heap.push(el, el)
+    }
     return heap
   }
 
   [Symbol.iterator]() {
-    const next = () => {
-      if (this.#arr.length > 0) {
-        return { value: this.pop(), done: false }
-      }
-      return { done: true }
+    function* elements(self) {
+      while (self.#arr.length > 0) { yield self.pop() }
     }
-    return { next }
+    return elements(this)
   }
 
-  [inspect.custom]() { return `Heap(${[...this]})` }
+  [inspect.custom]() {
+    return `Heap(${[...this].map(el => el.join(": ")).join(", ")})`
+  }
 
   // O(log(n)) pushes an element to a heap
-  push(data) {
-    this.#arr.push(data)
+  push(key, data) {
+    this.#arr.push([key, data])
     let i = this.#arr.length - 1
     while (i > 0) {
       const par = Math.floor((i - 1) / 2)
-      if (this.#cmp(this.#arr[i], this.#arr[par])) { break }
+      if (this.#cmp(this.#arr[i][0], this.#arr[par][0])) { break }
       arrSwap(this.#arr, i, par)
       i = par
     }
@@ -43,18 +44,18 @@ export class Heap {
   // O(log(n)) pops a max/min element from a heap
   pop() {
     if (this.#arr.length === 0) { error("pop from empty heap") }
-    const data = this.#arr[0]
+    const el = this.#arr[0]
     this.#arr[0] = this.#arr.at(-1)
     --this.#arr.length
     let par = 0, ch1 = par * 2 + 1, ch2 = ch1 + 1
     while (ch1 < this.#arr.length) {
-      const i = this.#arr[ch2] && this.#cmp(this.#arr[ch1], this.#arr[ch2]) ?
-            ch2 : ch1
-      if (this.#cmp(this.#arr[i], this.#arr[par])) { break }
+      const i = this.#arr[ch2] &&
+            this.#cmp(this.#arr[ch1][0], this.#arr[ch2][0]) ? ch2 : ch1
+      if (this.#cmp(this.#arr[i][0], this.#arr[par][0])) { break }
       arrSwap(this.#arr, i, par)
       par = i; ch1 = par * 2 + 1; ch2 = ch1 + 1
     }
-    return data
+    return el
   }
 
   // O(1) peeks a max/min element from a heap
--- ads/heap.test.js
+++ ads/heap.test.js
@@ -3,12 +3,14 @@ import { Heap } from "./heap.js"
 
 describe("Heap", () => {
   test("Heap from/length/iterator/push/pop/peek", () => {
+    const key = ([key, _]) => key
     const heap = Heap.from([11, 27, 17, 19, 15, 17, 31, 40])
     expect(heap.length).toBe(8)
-    expect(heap.peek()).toBe(40)
-    expect([...heap]).toEqual([40, 31, 27, 19, 17, 17, 15, 11])
+    expect(heap.peek()[0]).toBe(40)
+    expect([...heap].map(key)).toEqual([40, 31, 27, 19, 17, 17, 15, 11])
     expect(() => heap.peek()).toThrowError("peek from empty heap")
     expect(() => heap.pop()).toThrowError("pop from empty heap")
-    expect([...Heap.from([1, 3, 2], (a, b) => a > b)]).toEqual([1, 2, 3])
+    const heap2 = Heap.from([[1, "a"], [3, "c"], [2, "b"]], (a, b) => a > b)
+    expect([...heap2]).toEqual([[1, "a"], [2, "b"], [3, "c"]])
   })
 })
