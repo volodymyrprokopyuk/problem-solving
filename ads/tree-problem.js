3f85cbad87d0e7a06c0638cb0030bc41502282f7
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2023-09-27 10:07:23 +0200

    feat(ads): add univalCount

--- ads/tree-problem.js
+++ ads/tree-problem.js
@@ -1,5 +1,5 @@
 import { error } from "./util.js"
-import { BSTree } from "./tree.js"
+import { TNode, BSTree } from "./tree.js"
 
 // O(n) return a height of a tree
 export function height(nd) {
@@ -23,8 +23,38 @@ export function externalNodes(nd) {
     externalNodes(nd.left) + externalNodes(nd.right) : 0
 }
 
-// const bst = BSTree.from([19, 16, 24, 17, 20, 11, 28, 12, 27, 13])
-// console.log(height(bst.root))
-// console.log(nodes(bst.root))
-// console.log(internalNodes(bst.root))
-// console.log(externalNodes(bst.root))
+// O(n^2) counts the number of unival subtrees in a tree
+export function univalCount(nd) {
+  function isUnival(nd, val = nd.key) {
+    return !nd ? true :
+      nd.key === val ? isUnival(nd.left, val) && isUnival(nd.right, val) : false
+  }
+  return !nd ? 0 : univalCount(nd.left) + univalCount(nd.right) +
+    (isUnival(nd) ? 1 : 0)
+}
+
+// O(n) counts the number of unival subtrees in a tree
+export function univalCount2(nd) {
+  function uvCount(nd) {
+    if (!nd) { return true }
+    const luv = uvCount(nd.left), ruv = uvCount(nd.right)
+    return !luv || !ruv ? false :
+      nd.left && nd.left.key !== nd.key ? false :
+      nd.right && nd.right.key !== nd.key ? false :
+      (++count, true)
+  }
+  let count = 0
+  uvCount(nd)
+  return count
+}
+
+const n1 = new TNode(0), n2 = new TNode(1), n3 = new TNode(0),
+      n4 = new TNode(1), n5 = new TNode(0), n6 = new TNode(1),
+      n7 = new TNode(1), n8 = new TNode(0)
+n1.left = n2; n1.right = n3
+n3.left = n4; n3.right = n5
+n4.left = n6; n4.right = n7
+n5.right = n8;
+
+// console.log(univalCount(n1))
+// console.log(univalCount2(n1))
--- ads/tree-problem.test.js
+++ ads/tree-problem.test.js
@@ -1,7 +1,7 @@
 import { describe, test, expect } from "vitest"
-import { BSTree } from "./tree.js"
+import { TNode, BSTree } from "./tree.js"
 import {
-  height, nodes, internalNodes, externalNodes
+  height, nodes, internalNodes, externalNodes, univalCount, univalCount2
 } from "./tree-problem.js"
 
 describe("BSTree algorithms", () => {
@@ -24,4 +24,17 @@ describe("BSTree algorithms", () => {
     const bst = BSTree.from([19, 16, 24, 17, 20, 11, 28, 12, 27, 13])
     expect(externalNodes(bst.root)).toBe(4)
   })
+})
+
+describe("univalCount", () => {
+  const n1 = new TNode(0), n2 = new TNode(1), n3 = new TNode(0),
+        n4 = new TNode(1), n5 = new TNode(0), n6 = new TNode(1),
+        n7 = new TNode(1), n8 = new TNode(0)
+  n1.left = n2; n1.right = n3
+  n3.left = n4; n3.right = n5
+  n4.left = n6; n4.right = n7
+  n5.right = n8;
+
+  test("univalCount", () => { expect(univalCount(n1)).toBe(6) })
+  test("univalCount2", () => { expect(univalCount2(n1)).toBe(6) })
 })
