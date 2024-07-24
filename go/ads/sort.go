418e83821913a773ff9eeefe6e6abaf2569d25c4
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2024-07-24 10:58:15 +0200

    go: add sort tests

--- go/ads/sort.go
+++ go/ads/sort.go
@@ -1,4 +1,4 @@
-package main
+package ads
 
 import "fmt"
 
---
+++ go/ads/sort_test.go
@@ -0,0 +1,74 @@
+package ads_test
+
+import (
+  "testing"
+  "github.com/volodymyrprokopyuk/problem-solving/go/ads"
+)
+
+type tcase struct {
+  name string
+  arr []int
+  exp []int
+}
+
+func slcEqual(a, b []int) bool {
+  if len(a) != len(b) {
+    return false
+  }
+  for i := 0; i < len(a); i++ {
+    if a[i] != b[i] {
+      return false
+    }
+  }
+  return true
+}
+
+func cases() []tcase {
+  return []tcase{
+    {"empty array", []int{}, []int{}},
+    {"singleton array", []int{1}, []int{1}},
+    {"sorted array", []int{1, 2, 3}, []int{1, 2, 3}},
+    {"unordered array", []int{3, 1, 2}, []int{1, 2, 3}},
+    {
+      "duplicate elements",
+      []int{3, 1, 4, 9, 2, 8, 5, 6, 7, 9, 0},
+      []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9},
+    },
+  }
+}
+
+func TestBubbleSort(t *testing.T) {
+  for _, c := range cases() {
+    ads.BubbleSort(c.arr)
+    if !slcEqual(c.arr, c.exp) {
+      t.Errorf("slice is not sorted: expected %v, got %v", c.exp, c.arr)
+    }
+  }
+}
+
+func TestInsertSort(t *testing.T) {
+  for _, c := range cases() {
+    ads.InsertSort(c.arr)
+    if !slcEqual(c.arr, c.exp) {
+      t.Errorf("slice is not sorted: expected %v, got %v", c.exp, c.arr)
+    }
+  }
+}
+
+func TestShellSort(t *testing.T) {
+  for _, c := range cases() {
+    ads.ShellSort(c.arr)
+    if !slcEqual(c.arr, c.exp) {
+      t.Errorf("slice is not sorted: expected %v, got %v", c.exp, c.arr)
+    }
+  }
+}
+
+func TestSelectSort(t *testing.T) {
+  for _, c := range cases() {
+    ads.SelectSort(c.arr)
+    if !slcEqual(c.arr, c.exp) {
+      t.Errorf("slice is not sorted: expected %v, got %v", c.exp, c.arr)
+    }
+  }
+}
