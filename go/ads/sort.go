67f0aafd70b8705cf73f222e95f08f9fe0203736
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2024-07-24 15:19:15 +0200

    go: add Search

--- go/ads/sort.go
+++ go/ads/sort.go
@@ -1,10 +1,7 @@
-// package main
 package ads
 
-import "fmt"
-
 // O(n^2), in-place, stable, moving target
-func BubbleSort[T ~int | ~string](slc []T) {
+func BubbleSort(slc []int) {
   for i := len(slc) - 1; i > 0; i-- {
     swap := false
     for j := 0; j < i; j++ {
@@ -20,7 +17,7 @@ func BubbleSort[T ~int | ~string](slc []T) {
 }
 
 // O(n^2), in-place, stable, fixed target, O(n) on sorted array
-func InsertSort[T ~int | ~string](slc []T) {
+func InsertSort(slc []int) {
   for i := 1; i < len(slc); i++ {
     e := slc[i]
     j := i
@@ -35,7 +32,7 @@ func InsertSort[T ~int | ~string](slc []T) {
 }
 
 // O(n*log(n)), in-place, non-stable, generalization of InsertSort
-func ShellSort[T ~int | ~string](slc []T) {
+func ShellSort(slc []int) {
   for _, gap := range []int{31, 15, 7, 3, 1} {
     for i := gap; i < len(slc); i++ {
       for j := i; j - gap + 1 > 0 && slc[j] < slc[j - gap]; j-- {
@@ -46,7 +43,7 @@ func ShellSort[T ~int | ~string](slc []T) {
 }
 
 // O(n^2), in-place, non-stable, fixed target
-func SelectSort[T ~int | ~string](slc []T) {
+func SelectSort(slc []int) {
   for i := 0; i < len(slc) - 1; i++ {
     m := i
     for j := i + 1; j < len(slc); j++ {
@@ -117,13 +114,18 @@ func QuickSort(slc []int) {
   sort(0, len(slc))
 }
 
-func main() {
-  slc := []int{3, 1, 4, 9, 2, 8, 5, 6, 7, 9, 0}
-  // BubbleSort(slc)
-  // InsertSort(slc)
-  // ShellSort(slc)
-  // SelectSort(slc)
-  QuickSort(slc)
-  fmt.Println(slc)
-  // fmt.Println(MergeSort(slc))
+// O(log(n)), binary search of an ordered slice
+func Search(slc []int, val int) int {
+  a, b := 0, len(slc)
+  for a < b {
+    m := (a + b - 1) / 2
+    if val < slc[m] {
+      b = m
+    } else if val > slc[m] {
+      a = m + 1
+    } else {
+      return m
+    }
+  }
+  return -1
 }
--- go/ads/sort_test.go
+++ go/ads/sort_test.go
@@ -20,16 +20,16 @@ func slcEqual(a, b []int) bool {
 
 type tcase struct {
   name string
-  arr []int
+  slc []int
   exp []int
 }
 
 func cases() []tcase {
   return []tcase{
-    {"empty array", []int{}, []int{}},
-    {"singleton array", []int{1}, []int{1}},
-    {"sorted array", []int{1, 2, 3}, []int{1, 2, 3}},
-    {"unordered array", []int{3, 1, 2}, []int{1, 2, 3}},
+    {"empty slice", []int{}, []int{}},
+    {"singleton slice", []int{1}, []int{1}},
+    {"sorted slice", []int{1, 2, 3}, []int{1, 2, 3}},
+    {"unsorted slice", []int{3, 1, 2}, []int{1, 2, 3}},
     {
       "duplicate elements",
       []int{3, 1, 4, 9, 2, 8, 5, 6, 7, 9, 0},
@@ -40,54 +40,79 @@ func cases() []tcase {
 
 func TestBubbleSort(t *testing.T) {
   for _, c := range cases() {
-    ads.BubbleSort(c.arr)
-    if !slcEqual(c.arr, c.exp) {
-      t.Errorf("slice is not sorted: expected %v, got %v", c.exp, c.arr)
+    ads.BubbleSort(c.slc)
+    if !slcEqual(c.slc, c.exp) {
+      t.Errorf("%v: expected %v, got %v", c.name, c.exp, c.slc)
     }
   }
 }
 
 func TestInsertSort(t *testing.T) {
   for _, c := range cases() {
-    ads.InsertSort(c.arr)
-    if !slcEqual(c.arr, c.exp) {
-      t.Errorf("slice is not sorted: expected %v, got %v", c.exp, c.arr)
+    ads.InsertSort(c.slc)
+    if !slcEqual(c.slc, c.exp) {
+      t.Errorf("%v: expected %v, got %v", c.name, c.exp, c.slc)
     }
   }
 }
 
 func TestShellSort(t *testing.T) {
   for _, c := range cases() {
-    ads.ShellSort(c.arr)
-    if !slcEqual(c.arr, c.exp) {
-      t.Errorf("slice is not sorted: expected %v, got %v", c.exp, c.arr)
+    ads.ShellSort(c.slc)
+    if !slcEqual(c.slc, c.exp) {
+      t.Errorf("%v: expected %v, got %v", c.name, c.exp, c.slc)
     }
   }
 }
 
 func TestSelectSort(t *testing.T) {
   for _, c := range cases() {
-    ads.SelectSort(c.arr)
-    if !slcEqual(c.arr, c.exp) {
-      t.Errorf("slice is not sorted: expected %v, got %v", c.exp, c.arr)
+    ads.SelectSort(c.slc)
+    if !slcEqual(c.slc, c.exp) {
+      t.Errorf("%v: expected %v, got %v", c.name, c.exp, c.slc)
     }
   }
 }
 
 func TestQuickSort(t *testing.T) {
   for _, c := range cases() {
-    ads.QuickSort(c.arr)
-    if !slcEqual(c.arr, c.exp) {
-      t.Errorf("slice is not sorted: expected %v, got %v", c.exp, c.arr)
+    ads.QuickSort(c.slc)
+    if !slcEqual(c.slc, c.exp) {
+      t.Errorf("%v: expected %v, got %v", c.name, c.exp, c.slc)
     }
   }
 }
 
 func TestMergeSort(t *testing.T) {
   for _, c := range cases() {
-    got := ads.MergeSort(c.arr)
+    got := ads.MergeSort(c.slc)
     if !slcEqual(got, c.exp) {
-      t.Errorf("slice is not sorted: expected %v, got %v", c.exp, got)
+      t.Errorf("%v: expected %v, got %v", c.name, c.exp, got)
+    }
+  }
+}
+
+func TestSearch(t *testing.T) {
+  cases := []struct{
+    name string
+    slc []int
+    val int
+    exp int
+  }{
+    {"empty slice", []int{}, 9, -1},
+    {"singleton slice found", []int{1}, 1, 0},
+    {"singleton slice not found", []int{1}, 9, -1},
+    {"duplicate elements found", []int{1, 2, 2, 3}, 2, 1},
+    {"larger slice found first", []int{1, 2, 3, 4}, 1, 0},
+    {"larger slice found middle", []int{1, 2, 3, 4}, 2, 1},
+    {"larger slice found middle center", []int{1, 2, 3, 4, 5}, 3, 2},
+    {"larger slice found last", []int{1, 2, 3, 4}, 4, 3},
+    {"larger slice not found", []int{1, 2, 3, 4}, 9, -1},
+  }
+  for _, c := range cases {
+    got := ads.Search(c.slc, c.val)
+    if got != c.exp {
+      t.Errorf("%v: expected %v, got %v", c.name, c.exp, got)
     }
   }
 }
