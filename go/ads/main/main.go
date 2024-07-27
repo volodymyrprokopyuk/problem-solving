b5a6162d3aa47caf85eace0b72f39b926efc8ff7
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2024-07-27 19:06:37 +0200

    go: add Deque

--- go/ads/main/main.go
+++ go/ads/main/main.go
@@ -7,20 +7,33 @@ import (
 )
 
 func main() {
-  var stk ads.Stack
-  stk.FromSlice([]int{1, 2, 3})
-  val, _ := stk.Peek()
-  fmt.Println(val)
-  for stk.Length() > 0 {
-    val, _ := stk.Pop()
+  var deq ads.Deque
+  deq.FromSlice([]int{1, 2, 3})
+  for deq.Length() > 0 {
+    val, _ := deq.DeqFront()
     fmt.Println(val)
   }
-  stk.Push(10)
-  stk.Push(20)
-  for stk.Length() > 0 {
-    val, _ := stk.Pop()
+  deq.EnqFront(10)
+  deq.EnqFront(20)
+  for deq.Length() > 0 {
+    val, _ := deq.DeqRear()
     fmt.Println(val)
   }
+
+  // var stk ads.Stack
+  // stk.FromSlice([]int{1, 2, 3})
+  // val, _ := stk.Peek()
+  // fmt.Println(val)
+  // for stk.Length() > 0 {
+  //   val, _ := stk.Pop()
+  //   fmt.Println(val)
+  // }
+  // stk.Push(10)
+  // stk.Push(20)
+  // for stk.Length() > 0 {
+  //   val, _ := stk.Pop()
+  //   fmt.Println(val)
+  // }
 
   // var que ads.Queue
   // que.FromSlice([]int{1, 2, 3})
--- go/ads/queue.go
+++ go/ads/queue.go
@@ -28,3 +28,41 @@ func (q *Queue) Peek() (int, error) {
 func (q *Queue) Deq() (int, error) {
   return q.lst.PopHead()
 }
+
+type Deque struct {
+  lst DList
+}
+
+func (d *Deque) Length() int {
+  return d.lst.Length()
+}
+
+func (d *Deque) FromSlice(slc []int) {
+  for _, val := range slc {
+    d.EnqRear(val)
+  }
+}
+
+func (d *Deque) EnqFront(val int) {
+  d.lst.PushHead(val)
+}
+
+func (d *Deque) EnqRear(val int) {
+  d.lst.PushTail(val)
+}
+
+func (d *Deque) PeekFront() (int, error) {
+  return d.lst.PeekHead()
+}
+
+func (d *Deque) PeekRear() (int, error) {
+  return d.lst.PeekTail()
+}
+
+func (d *Deque) DeqFront() (int, error) {
+  return d.lst.PopHead()
+}
+
+func (d *Deque) DeqRear() (int, error) {
+  return d.lst.PopTail()
+}
