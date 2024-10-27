9ff8490372d10b2e144f47bf4a750fb1717c7083
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2024-10-27 16:30:13 +0100

    sol: add Counter tests

--- solidity/test/Counter.t.sol
+++ solidity/test/Counter.t.sol
@@ -7,12 +7,7 @@ import {Counter} from "contract/Counter.sol";
 contract CounterTest is Test {
   Counter public counter;
 
-  constructor() {
-    console.log("=> before all");
-  }
-
   function setUp() public {
-    console.log("=> before each");
     counter = new Counter();
     counter.setNumber(0);
   }
@@ -20,6 +15,24 @@ contract CounterTest is Test {
   function testIncrement() public {
     counter.increment();
     assertEq(counter.number(), 1);
+  }
+
+  function testFailNonOwnerIncrement() public {
+    vm.prank(address(0));
+    counter.increment();
+  }
+
+  function testRevertNonOwnerIncrement() public {
+    vm.expectRevert(Counter.ErrOnlyOwner.selector);
+    vm.prank(address(0));
+    counter.increment();
+  }
+
+  function testEmitIncrement() public {
+    // indexed topic1, topic2, topic3, non-indexed data
+    vm.expectEmit(true, false, false, true);
+    emit Counter.EvIncrement(1); // expected event
+    counter.increment(); // actual event
   }
 
   function testFailDoRevert() public view {
@@ -32,35 +45,37 @@ contract CounterTest is Test {
   }
 }
 
-abstract contract Shared {
+abstract contract SharedSetup {
   uint256 public constant number = 1234;
 }
 
-contract MultiTxTest is Test, Shared {
+contract MultiTxTest is Test, SharedSetup {
   uint256 a;
   uint256 b;
 
   function beforeTestSetup(bytes4 testSelector) public pure
     returns (bytes[] memory) {
-    if (testSelector == this.testC.selector) {
+    if (testSelector == this.testMultiTx.selector) {
       bytes[] memory testCalldata = new bytes[](2);
-      testCalldata[0] = abi.encodePacked(this.testA.selector);
-      testCalldata[1] = abi.encodeWithSignature("setB(uint256)", number);
+      testCalldata[0] = abi.encodePacked(this.testStateless.selector);
+      testCalldata[1] = abi.encodeWithSignature("setUpMultiTx(uint256)", number);
       return testCalldata;
     }
     return new bytes[](0);
   }
 
-  function testA() public {
+  function testStateless() public {
+    console.log("=> testA");
     require(a == 0);
     a = 1;
   }
 
-  function setB(uint256 value) public {
+  function setUpMultiTx(uint256 value) public {
+    console.log("=> setUpMultiTx");
     b = value;
   }
 
-  function testC() public view {
+  function testMultiTx() public view {
     assertEq(a, 1);
     assertEq(b, number);
   }
